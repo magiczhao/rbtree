d7f82324bb89c22f564a39e288834d3c11a83cae
magiczhao <magic.demon.zhao@gmail.com>
2015-04-06 00:26:45 +0800

    Add rotate

--- rbtree.go
+++ rbtree.go
@@ -1,128 +1,166 @@
 package rbtree
 
+import (
+	"errors"
+)
+
 type RbColor bool
 
-func (c RbColor) IsRed() {
-    return c
+func (c RbColor) IsRed() bool {
+	return c == true
 }
 
-func (c RbColor) IsBlack() {
-    return !c
+func (c RbColor) IsBlack() bool {
+	return c == false
 }
 
 func (c *RbColor) SetBlack() {
-    *c = false
+	*c = false
 }
 
 func (c *RbColor) SetRed() {
-    *c = true
+	*c = true
 }
 
 type Comparable interface {
-    Less(Comparable) bool
+	Less(Comparable) bool
 }
 
-type NInf struct {}
+type NInf struct{}
 
-func (n NInf) Less(data Comparable) {
-    return true
+func (n NInf) Less(data Comparable) bool {
+	return true
 }
 
 var (
-    ninf = NInf{}
-    ErrorNotFound = errors.New("value not found")
+	ninf               = NInf{}
+	ErrorNotFound      = errors.New("value not found")
+	ErrorAlreadyExists = errors.New("Node already exists")
 )
 
 type rbNode struct {
-    // tree struct
-    parent *rbNode
-    left *rbNode
-    right *rbNode
-    // color of the node
-    color RbColor
-    // key and value are stored together
-    key Comparable
+	// tree struct
+	parent *rbNode
+	left   *rbNode
+	right  *rbNode
+	// color of the node
+	color RbColor
+	// key and value are stored together
+	key Comparable
 }
 
 type RbTree struct {
-    root *rbNode
+	root *rbNode
 }
 
 func (t RbTree) Max() Comparable {
-    if t.root == nil {
-        return nil
-    }
-    node := t.root
-    for node.right != nil {
-        node = node.right
-    }
-    return node.key
+	if t.root == nil {
+		return nil
+	}
+	node := t.root
+	for node.right != nil {
+		node = node.right
+	}
+	return node.key
 }
 
 func (t RbTree) Min() Comparable {
-    if t.root == nil {
-        return nil
-    }
-    node := t.root
-    for node.left != nil {
-        node = node.left
-    }
-    return node.key
+	if t.root == nil {
+		return nil
+	}
+	node := t.root
+	for node.left != nil {
+		node = node.left
+	}
+	return node.key
 }
 
 func newRbNode(data Comparable) *rbNode {
-    node := rbNode{nil, nil, nil, true, data}
-    return &node
+	node := rbNode{nil, nil, nil, true, data}
+	return &node
 }
 
 func (t *RbTree) Insert(data Comparable) error {
-    node := t.findPosition(data)
-    if node == nil {
-        // empty tree
-        t.root = newRbNode(data)
-        return nil
-    }
-    nNode := newRbNode(data)
-    nNode.color.SetRed()
-    if node.key.Less(data) {
-        node.right = nNode
-    } else {
-        node.left = nNode
-    }
-    if node.color.IsBlack() {
-        return nil
-    }
-    return nil
+	node := t.findPosition(data)
+	if node == nil {
+		// empty tree
+		t.root = newRbNode(data)
+		return nil
+	}
+	nNode := newRbNode(data)
+	nNode.color.SetRed()
+	if node.key.Less(data) {
+		node.right = nNode
+	} else if data.Less(node.key) {
+		node.left = nNode
+	} else {
+		// already in tree
+		return ErrorAlreadyExists
+	}
+	if node.color.IsBlack() {
+		return nil
+	}
+	return nil
 }
 
 func (t RbTree) Find(data Comparable) (Comparable, error) {
-    node := t.findPosition(data)
-    if node == nil {
-        return ninf, ErrorNotFound
-    }
-    // Equal
-    if !data.Less(node) && !node.key.Less(data) {
-        return node.key
-    }
-    return ninf, ErrorNotFound
+	node := t.findPosition(data)
+	if node == nil {
+		return ninf, ErrorNotFound
+	}
+	// Equal
+	if !data.Less(node.key) && !node.key.Less(data) {
+		return node.key, nil
+	}
+	return ninf, ErrorNotFound
+}
+
+func (t *RbTree) rotateLeft(node *rbNode) {
+	parent := node.parent
+	if node.right == nil {
+		return
+	}
+	right := node.right
+	node.right = right.left
+	right.left = node
+	if node == parent.left {
+		parent.left = right
+	} else {
+		parent.right = right
+	}
+}
+
+func (t *RbTree) rotateRight(node *rbNode) {
+	parent := node.parent
+	if node.left == nil {
+		return
+	}
+	left := node.left
+	node.left = left.right
+	left.right = node
+	if node == parent.left {
+		parent.left = left
+	} else {
+		parent.right = left
+	}
 }
 
 func (t RbTree) findPosition(data Comparable) *rbNode {
-    if t.root == nil {
-        return nil
-    }
-    node := t.root
-    parent := node
-    for node != nil {
-        parent = node
-        if data.Less(node.key) {
-            node = node.left
-        } else if node.key.Less(data) {
-            node = node.right
-        } else {
-            // node is the target
-            return node
-        }
-    }
-    return parent
+	if t.root == nil {
+		return nil
+	}
+	node := t.root
+	parent := node
+	for node != nil {
+		parent = node
+		if data.Less(node.key) {
+			node = node.left
+		} else if node.key.Less(data) {
+			node = node.right
+		} else {
+			// node is the target
+			return node
+		}
+	}
+	return parent
 }
