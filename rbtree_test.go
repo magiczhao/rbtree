4b38b01140e88fa7d2767a195d77c24e4b51b2a7
magiczhao <magic.demon.zhao@gmail.com>
2015-04-07 00:23:00 +0800

    add unit test

--- rbtree_test.go
+++ rbtree_test.go
@@ -1,1 +1,32 @@
 package rbtree
+
+import (
+	"testing"
+)
+
+type IntValue int
+
+func (i IntValue) Less(val Comparable) bool {
+	return i < val.(IntValue)
+}
+
+func TestRbTreeFind(t *testing.T) {
+	tree := RbTree{}
+	var ival IntValue
+	ival = 10
+	tree.root = newRbNode(ival)
+	ival = 6
+	tree.root.left = newRbNode(ival)
+	val, err := tree.Find(ival)
+	if err != nil {
+		t.Error("Failed to find", ival)
+	}
+	if val != ival {
+		t.Error("Wrong value")
+	}
+	ival = 5
+	_, err = tree.Find(ival)
+	if err == nil {
+		t.Error("Should not find item")
+	}
+}
